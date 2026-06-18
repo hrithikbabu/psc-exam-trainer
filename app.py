1f37ed241c6ab84547f757493218bf52790a3ef4
hrithikbabu <hrithikbabu37@gmail.com>
2026-06-18 10:12:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -345,4 +345,4 @@ def manage_users():
     return render_template("admin/users.html", users=users)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
